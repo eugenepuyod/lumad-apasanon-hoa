aef770743122a9c4d54553caa0eed0304c369391
Eugene Puyod <eugenerpuyod@gmail.com>
2026-05-02 21:19:54 +0800

    Initial commit

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -4,7 +4,7 @@ import { Calendar as CalendarIcon, MapPin, Clock } from 'lucide-vue-next'
 const events = [
   {
     id: 1,
-    title: 'Annual General Meeting',
+    title: 'General Meeting',
     date: 'May 08, 2026',
     time: '6:00 PM - 8:00 PM',
     location: 'Sitio Central Apas, Barangay Apas Gym',
